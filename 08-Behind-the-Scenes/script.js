889d860088e9b55e3d253324e800e037ec6d0a7f
Leandro Reis <leandro.reiz@gmail.com>
2021-12-30 22:35:41 +0000

    JavaScript Hoisting

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,8 +1,7 @@
+'use strict';
+/*
 ///////////////////////////////////
 // SCOPE IN JAVASCRIPT
-
-'use strict';
-
 const currentYear = new Date().getFullYear();
 
 function calcAge(birthYear) {
@@ -42,3 +41,57 @@ const firstName = 'Leandro';
 calcAge(1983);
 // console.log(age); // reference error
 // printAge(); // reference error
+*/
+///////////////////////////////////
+// HOISTING
+
+// Variables
+console.log(me); // hoisted to the value 'undefined'
+
+// Cannot access 'job' before initialization
+// console.log(job);
+// console.log(birthYear);
+
+var me = 'Leandro';
+let job = 'Coder';
+const birthYear = 1983;
+
+// Functions
+
+// console.log(addDeclaration(2, 3));
+// 5
+
+// console.log(addExpression(2, 3));
+// Cannot access 'addExpression' before initialization
+
+// console.log(addArrow(2, 3));
+// addArrow is not a function
+
+function addDeclaration(a, b) {
+  return a + b;
+}
+
+const addExpression = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Another example
+// numProducts at this point is undefined
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// More About Variables
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
