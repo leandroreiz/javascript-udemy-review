5701c2986f907f462441547a7d6f2f2821721df5
Leandro Reis <leandro.reiz@gmail.com>
2022-01-04 20:47:08 +0000

    IIFE & Closures

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -107,7 +107,7 @@ const greeterHey = greet('Hey');
 greeterHey('Chelem');
 
 greet('Holla')('Hermano'); // use and call the function at the same
-*/
+
 //////////////////////////////////////////
 // THE CALL AND APPLY METHODS
 
@@ -199,3 +199,92 @@ console.log(addVAT(100));
 // Challenge
 const purchaseTotal = value => rate => value + value * rate;
 console.log(purchaseTotal(1000)(0.23));
+
+//////////////////////////////////////////////////
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+// Normal function
+const runOnce = function () {
+  console.log('This will run always when called');
+};
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  // const isPrivate = 77; // local variable
+})();
+
+(() => console.log(`this will ALSO never run again`))();
+
+// console.log(isPrivate); // is not defined in global scope
+
+{
+  const isPrivate = 77;
+  var notPrivate = 154;
+}
+
+// console.log(isPrivate); // is not defined in global scope
+console.log(notPrivate); // 154, as it is declared using 'var'
+
+//////////////////////////////////////////////////
+// CLOSURES
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker); // [[Scopes]] => Closure (secureBooking)
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 77;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // 46
+console.dir(f);
+
+// Re-assigning f function
+h();
+f(); // 154
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  // Setting the timer
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
