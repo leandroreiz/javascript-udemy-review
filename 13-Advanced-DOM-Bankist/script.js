dc37650b2f10eaddf581684fb5de1df1239ddac6
Leandro Reis <leandro.reiz@gmail.com>
2022-01-12 20:13:43 +0000

    Working with Attributes, Styles and Classes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -36,6 +36,8 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 // LECTURES
 
+///////////////////////////////////////
+// SELECTING ELEMENTS
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -57,7 +59,6 @@ console.log(document.getElementsByClassName('btn'));
 ///////////////////////////////////////
 // CREATING AND INSERTING ELEMENTS
 // .insertAdjacentHTML
-
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = `We use cookies for improved functionality and analytics.`;
@@ -65,7 +66,6 @@ message.innerHTML = `We use cookies for improved functionality and analytics. <b
 // header.prepend(message);
 // header.append(message.cloneNode(true));
 header.append(message);
-
 // header.before(message);
 // header.after(message);
 
@@ -76,3 +76,52 @@ document
   .addEventListener('click', () => message.remove());
 // Old way to remove an element would be selecting its parent
 // .addEventListener('click', () => message.parentElement.removeChild(message));
+
+///////////////////////////////////////
+// STYLES
+message.style.backgroundColor = '#37383d';
+message.style.width = '104%';
+
+console.log(message.style.color); // nothing is returned as it wasn't set as inline style
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187)
+console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+///////////////////////////////////////
+// ATTRIBUTES
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+// Non-standard property
+console.log(logo.designer); // nothing is return
+console.log(logo.getAttribute('designer'));
+// --
+
+// Set attributes
+logo.alt = `Beautiful minimalist logo`;
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src); // http://127.0.0.1:8080/img/logo.png
+console.log(logo.getAttribute('src')); // img/logo.png
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // http://127.0.0.1:8080/#
+console.log(link.getAttribute('href')); // #
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('a', 'b');
+logo.classList.remove();
+logo.classList.toggle();
+logo.classList.contains();
+
+// remove all other classes
+// logo.className = 'DontUseThis';
