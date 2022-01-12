2e393e615a4bb55cf6759b49358030a99f8a7ca8
Leandro Reis <leandro.reiz@gmail.com>
2022-01-12 19:03:27 +0000

    Creating, Inserting and Deleting Elements from the DOM

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -30,3 +30,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+// LECTURES
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+console.log(document.getElementById('section--1'));
+
+const header = document.querySelector('.header');
+
+// returns a node list
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+// --
+
+// returns a HTML collection
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+// --
+
+///////////////////////////////////////
+// CREATING AND INSERTING ELEMENTS
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = `We use cookies for improved functionality and analytics.`;
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+// header.prepend(message);
+// header.append(message.cloneNode(true));
+header.append(message);
+
+// header.before(message);
+// header.after(message);
+
+///////////////////////////////////////
+// DELETING ELEMENTS
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
+// Old way to remove an element would be selecting its parent
+// .addEventListener('click', () => message.parentElement.removeChild(message));
