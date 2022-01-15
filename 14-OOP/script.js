b996d491d1a4ac7cf176fd2430299a4aeea93785
Leandro Reis <leandro.reiz@gmail.com>
2022-01-15 20:12:09 +0000

    Challenge #1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//////////////////////////////////////////////
+// PROTOTYPES
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -69,3 +72,41 @@ console.log(arr.unique());
 const h1 = document.querySelector('h1');
 console.dir(h1);
 console.dir(x => x + 1);
+*/
+
+//////////////////////////////////////////////
+// CHALLENGE - OOP #1
+
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The ${this.make} is going at ${this.speed}km/h.`);
+};
+
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The ${this.make} is going at ${this.speed}km/h.`);
+};
+
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+/* Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h */
+
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+
+mercedes.accelerate();
+mercedes.brake();
