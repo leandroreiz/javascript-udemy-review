dd371baf322a8cffea337edfdebaac96a4c76a42
Leandro Reis <leandro.reiz@gmail.com>
2022-01-15 15:24:01 +0000

    Lifecycle DOM Events, Efficient Script Loading: defer and async

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -514,4 +514,19 @@ console.log(h1.parentElement.children);
     el.style.transform = 'scale(0.5)';
   }
 });
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log(`HTML parsed and DOM tree built!`, e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log(`Page fully loaded!`, e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(`Before unload!`, e);
+  e.returnValue = '';
+});
+
 */
