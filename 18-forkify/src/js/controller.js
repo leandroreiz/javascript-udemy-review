4f47307b70135e083c5995e036949f4ae7555467
Leandro Reis <leandro.reiz@gmail.com>
2022-01-24 22:17:37 +0000

    Loading a Recipe from API

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const apiKey = 'b1921daf-6113-4e87-97f4-137790063c1b';
+const getRecipeURL = 'https://forkify-api.herokuapp.com/api/v2/recipes/:id';
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+
+    if (!response.ok)
+      throw new Error(`Recipe not found! ${data.message} ${data.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(`🔴 ${err.message}`);
+  }
+};
+showRecipe();
