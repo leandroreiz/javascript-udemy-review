43e5d2cfecb3b4a1b0262d198f942150a7c938f0
Leandro Reis <leandro.reiz@gmail.com>
2022-01-30 10:53:59 +0000

    How to Add Documentation to JavaScript Code

--- 18-forkify/dist/index.e37f48ea.js
+++ 18-forkify/dist/index.e37f48ea.js
@@ -2345,7 +2345,14 @@ var _iconsSvg = require("url:../../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class View {
     _data;
-    render(data, render = true) {
+    /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data do be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Leandro Reis
+   */ render(data, render = true) {
         if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const markup = this._generateMarkup();
